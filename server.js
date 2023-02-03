20fe4a229073ad160b869eadfa4b51e6bd1b59ab
RyanMcd29 <117286428+RyanMcd29@users.noreply.github.com>
2023-02-03 21:14:07 +0800

    added get responses for home page and notes page

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 const express = require('express');
-const api = require('./public/assets/js/index.js');
+// const api = require('./public/assets/js/index.js');
+const path = require('path')
 
 const PORT = process.env.PORT || 3001;
 
-const app = express;
+const app = express();
+
+app.get('/notes', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+app.get('/*', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.listen(PORT, () =>
+    console.log(`App now live at, http://localhost:${PORT}`)
+);
 
