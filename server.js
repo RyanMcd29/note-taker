589837982687544ce96be34b9526314160afc797
RyanMcd29 <117286428+RyanMcd29@users.noreply.github.com>
2023-02-07 16:06:27 +0800

    refactored files in js

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
-// const api = require('./public/assets/js/index.js');
+const api = require('./public/assets/routes/router.js');
 const path = require('path')
-
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+
+app.use('/api', api)
 
 app.get('/notes', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/notes.html'))
