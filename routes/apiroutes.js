5b1147128714dceb594ab987609a0b080e6dbeea
RyanMcd29 <117286428+RyanMcd29@users.noreply.github.com>
2023-02-08 22:21:31 +0800

    delete note request

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,6 +1,7 @@
 // const store = require('../db/store');
 const { randomUUID } = require('crypto');
-const fs = require('fs')
+const fs = require('fs');
+const app = require('./htmlroutes');
 const api = require('express').Router();
 
 api.get('/notes', (req, res) => {
@@ -23,7 +24,7 @@ api.post('/notes', (req, res) => {
         const newNote = {
             title,
             text,
-            noteID : randomUUID(),
+            id : randomUUID(),
         };
 
     // Obtain existing notes database
@@ -54,5 +55,33 @@ api.post('/notes', (req, res) => {
         res.status(500).json('error in posting note')
     }
 });
+
+api.delete('/notes/:id', (req, res) => {
+    // get notes database
+    console.info(`${req.method} request received to delete a note`)
+    let id = req.params.id
+    fs.readFile('db/db.json', 'utf8', (err, data) => {
+        if (err) {
+          console.error(err);
+        } else {
+    // filter notes that do not contain the id
+        const oldNotes = JSON.parse(data)
+            console.log(oldNotes)
+        const newNotes = oldNotes.filter((note) => note.id !== id);
+        console.log(newNotes)
+        fs.writeFile('db/db.json', JSON.stringify(newNotes), (err) => 
+                err
+                    ? console.error(err)
+                    : console.info('Successfully updated stored notes')
+                )   
+        res.json(newNotes)     
+        }
+    }
+    );
+   
+    
+    
+});
+
 
 module.exports = api
