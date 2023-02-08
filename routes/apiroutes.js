5a3a046dc8da480368d1875ddd0cde40777c3501
RyanMcd29 <117286428+RyanMcd29@users.noreply.github.com>
2023-02-08 20:48:35 +0800

    added post request

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,4 +1,5 @@
 // const store = require('../db/store');
+const { randomUUID } = require('crypto');
 const fs = require('fs')
 const api = require('express').Router();
 
@@ -10,6 +11,48 @@ api.get('/notes', (req, res) => {
         res.json(JSON.parse(data)) 
     }
     })      
+});
+
+api.post('/notes', (req, res) => {
+    console.info(`${req.method} request received to add a note`)
+    console.log(req.body);
+    // Destructuring assignment
+    const { title, text } = req.body;
+    // variable for the object to save
+    if (title && text) {
+        const newNote = {
+            title,
+            text,
+            noteID : randomUUID(),
+        };
+
+    // Obtain existing notes database
+    fs.readFile('db/db.json', 'utf8', (err, data) => {
+        if (err) {
+          console.error(err);
+        } else {
+            // import new notes into old notes
+            const oldNotes = JSON.parse(data)
+            oldNotes.push(newNote)
+
+            // write file
+            fs.writeFile('../db/db.json', JSON.stringify(oldNotes), (err) => 
+                err
+                    ? console.error(err)
+                    : console.info('Successfully updated stored notes')
+                )
+        }
+    });
+        const response = {
+        status: 'success',
+        body: newNote
+    };
+
+    console.log(response);
+            res.status(201).json(response)
+    } else {
+        res.status(500).json('error in posting note')
+    }
 });
 
 module.exports = api
